d7132a417f775c02cadfbcd7b26217f47a9652c5
11best <asama_b@outlook.com>
2023-12-11 21:01:32 +0700

    add updateBook handler

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	app.Get("/books", getBooks)
 	app.Get("/books/:id", getBook)
 	app.Post("/books", createBook)
+	app.Put("/books/:id", updateBook)
 
 	app.Listen(":8080")
 }
@@ -56,4 +57,27 @@ func createBook(c *fiber.Ctx) error {
 
 	books = append(books, *book)
 	return c.JSON(book)
+}
+
+func updateBook(c *fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	bookUpdate := new(Book)
+	if err := c.BodyParser(bookUpdate); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	for i, book := range books {
+		if book.ID == id {
+			book.Title = bookUpdate.Title
+			book.Author = bookUpdate.Author
+			books[i] = book
+			return c.JSON(book)
+		}
+	}
+
+	return c.SendStatus(fiber.StatusNotFound)
 }
