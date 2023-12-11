cc63b143a1a574072a1ee0ab5b8628cba5aefce3
11best <asamaporn.b@appman.co.th>
2023-12-11 17:08:07 +0700

    example for get book for struct

--- main.go
+++ main.go
@@ -1,15 +1,49 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 )
+
+type Book struct {
+	ID     int    `json:"id"` //`json:id`(metadata) its for fiber to map request and response to struct
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books []Book
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/hello", func(c *fiber.Ctx) error {
-		return c.SendString("hello world!")
-	})
+	books = append(books, Book{ID: 1, Title: "Papuan 101", Author: "Papuan"})
+	books = append(books, Book{ID: 2, Title: "Patoo", Author: "Papuan"})
+
+	app.Get("/books", getBooks)
+	app.Get("/books:id", getBook)
 
 	app.Listen(":8080")
+}
+
+func getBooks(c *fiber.Ctx) error {
+	return c.JSON(books)
+}
+
+func getBook(c *fiber.Ctx) error {
+	bookId, err := strconv.Atoi(c.Params("id"))
+
+	print(bookId)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	for _, book := range books {
+		if book.ID == bookId {
+			return c.JSON(book)
+		}
+	}
+
+	return c.SendStatus(fiber.StatusNotFound)
 }
