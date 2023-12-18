65fe93d4510f48ae7a9f4c3f1e58093dbb2bc502
11best <asamaporn.b@appman.co.th>
2023-12-19 00:44:19 +0700

    create simple middleware

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
@@ -31,6 +33,7 @@ func main() {
 	books = append(books, Book{ID: 1, Title: "Papuan 101", Author: "Papuan"})
 	books = append(books, Book{ID: 2, Title: "Patoo", Author: "Papuan"})
 
+	app.Use(checkMiddleware)
 	app.Get("/books", getBooks)
 	app.Get("/books/:id", getBook)
 	app.Post("/books", createBook)
@@ -40,7 +43,7 @@ func main() {
 
 	app.Get("/config", getEnv)
 
-	app.Listen(":8080")
+	app.Listen(":7777")
 }
 
 func sayHello(c *fiber.Ctx) error {
@@ -50,8 +53,14 @@ func sayHello(c *fiber.Ctx) error {
 }
 
 func getEnv(c *fiber.Ctx) error {
-
 	return c.JSON(fiber.Map{
 		"SECRET": os.Getenv("SECRET"),
 	})
+}
+
+func checkMiddleware(c *fiber.Ctx) error {
+	start := time.Now()
+
+	fmt.Printf("URL: %s, Method: %s, Time: %s\n", c.OriginalURL(), c.Method(), start)
+	return c.Next()
 }
