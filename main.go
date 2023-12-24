6de8a25990f662a453c6c99adb2aa5059bdc7320
11best <asamaporn.b@appman.co.th>
2023-12-24 23:58:25 +0700

    insert using go function

--- main.go
+++ main.go
@@ -16,15 +16,25 @@ const (
 	password     = "mypassword"
 )
 
+var db *sql.DB
+
+type Product struct {
+	ID    int
+	Name  string
+	Price int
+}
+
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, username, password, databaseName)
 
-	db, err := sql.Open("postgres", psqlInfo)
+	sdb, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	db = sdb
 
 	err = db.Ping()
 	if err != nil {
@@ -32,4 +42,21 @@ func main() {
 	}
 
 	print("Connection Database Successful!")
+
+	err = createProduct(&Product{Name: "platoo in go", Price: 404})
+	if err != nil {
+		log.Fatal(err)
+	}
+	print("create successful")
+
+}
+
+func createProduct(product *Product) error {
+	_, err := db.Exec(
+		"INSERT INTO public.products(name, price) VALUES ($1, $2);",
+		product.Name,
+		product.Price,
+	)
+
+	return err
 }
